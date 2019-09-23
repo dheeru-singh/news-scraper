05fd6ed3b5f2521e43f70b1b111a6e85b7617d5e
Dheeru Singh <dheeru.singh228@gmail.com>
2019-09-23 15:02:21 -0400

    initial commit

--- server.js
+++ server.js
@@ -27,7 +27,11 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/newsScraperdb", { useNewUrlParser: true });
+var MONGODB_URI= process.env.MONGODB_URI || "mongodb://localhost/newsScraperdb";
+
+mongoose.connect(MONGODB_URI);
+// mongoose.connect("mongodb://localhost/newsScraperdb", { useNewUrlParser: true });
+
 
 // Routes
 
