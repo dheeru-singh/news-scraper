9ffe0c46d8cdb781f46d0abda33a18fc41a9df53
Dheeru Singh <dheeru.singh228@gmail.com>
2019-09-24 11:29:33 -0400

    add data limit unique title

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.get("/scrape", function(req, res) {
 // Route for getting all Articles from the db
 app.get("/articles", function(req, res) {
   // Grab every document in the Articles collection
-  db.Article.find({})
+  db.Article.find({}).limit(10)
   .populate("note")
     .then(function(dbArticle) {
       // If we were able to successfully find Articles, send them back to the client
@@ -92,22 +92,6 @@ app.get("/articles", function(req, res) {
     });
 });
 
-
-// // Route for grabbing a specific Article by id, populate it with it's note
-// app.get("/articles/:id", function(req, res) {
-//   // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-//   db.Article.findOne({ _id: req.params.id })
-//     // ..and populate all of the notes associated with it
-//     .populate("note")
-//     .then(function(dbArticle) {
-//       // If we were able to successfully find an Article with the given id, send it back to the client
-//       res.json(dbArticle);
-//     })
-//     .catch(function(err) {
-//       // If an error occurred, send it to the client
-//       res.json(err);
-//     });
-// });
 
 // Route for saving/updating an Article's associated Note
 app.post("/articles/:id", function(req, res) {
